5125237759b9c9959c1d2499c83e7f658dbc6619
dingmeikun <ading0602@foxmail.com>
2019-07-09 17:05:49 +0800

    update config

--- springcloud-ribbon-client/src/main/java/com/dingmk/ribbon/config/RibbonConfig.java
+++ springcloud-ribbon-client/src/main/java/com/dingmk/ribbon/config/RibbonConfig.java
@@ -5,6 +5,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
+
 /**
  * 构造restTemplate负载均衡器
  * 
@@ -14,9 +17,22 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class RibbonConfig {
 
+	/**
+     * 实例化ribbon使用的RestTemplate
+     * @return
+     */
 	@Bean
 	@LoadBalanced
 	RestTemplate restTemplate() {
 		return new RestTemplate();
 	}
+	
+	/**
+	 * 配置随机负载策略，需要配置属性service-B.ribbon.NFLoadBalancerRuleClassName=com.netflix.loadbalancer.RandomRule
+	 */
+	 @Bean
+	 public IRule ribbonRule() {
+	     return new RandomRule();
+	 }
+	    
 }
