fd434b37b4a36bcf7ae5868785c724620716f260
dingmeikun <ading0602@foxmail.com>
2019-07-10 16:26:45 +0800

    add hystrix dashboard config

--- springcloud-eureka-feign/src/main/java/com/dingmk/eureka/EurekaFeign.java
+++ springcloud-eureka-feign/src/main/java/com/dingmk/eureka/EurekaFeign.java
@@ -3,13 +3,17 @@ package com.dingmk.eureka;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@EnableEurekaClient
+@EnableDiscoveryClient
+@EnableHystrixDashboard
+@EnableCircuitBreaker
 @EnableFeignClients
 @EnableHystrix
 @EnableScheduling
