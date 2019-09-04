1e9f70becadec64ed4a7eb65611596eb377275e8
丁美坤 <dingmeikun@mfexcel.com>
2019-09-04 18:24:55 +0800

    add muti repo config

---
+++ springcloud-config/springcloud-config-client-muti-repo/src/main/java/com/dingmk/config/controller/ClientConfigController.java
@@ -0,0 +1,23 @@
+package com.dingmk.config.controller;
+
+import javax.annotation.Resource;
+
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.dingmk.config.configure.ClinetConfigGitProperties;
+
+@RefreshScope
+@RestController
+@RequestMapping("configConsumer")
+public class ClientConfigController {
+
+	@Resource
+	private ClinetConfigGitProperties properties;
+	
+	@RequestMapping("/getconfigInfo")
+	public String getConfigInfo() {
+		return properties.getConfig();
+	}
+}
