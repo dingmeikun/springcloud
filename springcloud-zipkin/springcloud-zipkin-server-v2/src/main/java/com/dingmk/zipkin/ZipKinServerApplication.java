99efeac58fc24381677514a2ac93b2df6266d4da
丁美坤 <dingmeikun@mfexcel.com>
2019-10-24 10:52:25 +0800

    更新项目

--- springcloud-zipkin/springcloud-zipkin-server-v2/src/main/java/com/dingmk/zipkin/ZipKinServerApplication.java
+++ springcloud-zipkin/springcloud-zipkin-server-v2/src/main/java/com/dingmk/zipkin/ZipKinServerApplication.java
@@ -3,10 +3,12 @@ package com.dingmk.zipkin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 import zipkin2.server.internal.EnableZipkinServer;
 
 @SpringBootApplication
+@EnableEurekaClient
 @EnableDiscoveryClient
 @EnableZipkinServer
 public class ZipKinServerApplication {
