da70a200ef755b596b170c95df4ca18ec07df4d3
丁美坤 <dingmeikun@mfexcel.com>
2019-10-24 18:55:04 +0800

    更新配置

--- springcloud-admin/springcloud-admin-server/src/main/java/com/dingmk/admin/AdminApplication.java
+++ springcloud-admin/springcloud-admin-server/src/main/java/com/dingmk/admin/AdminApplication.java
@@ -9,6 +9,14 @@ import org.springframework.context.annotation.Configuration;
 
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 
+/**
+ * spring admin资源监控服务，可以对服务的健康状态、cpu、线程数、磁盘空间、堆站等作出一些收集，并提供页面查看
+ * 
+ * 风险点：admin默认收集当前注册中心eureka的所有实例，如果某个实例没有配置spring admin的相关信息，将收集不到，并且报错
+ * 
+ * @author dingmeikun
+ *
+ */
 @SpringBootApplication
 @EnableEurekaClient
 @EnableCircuitBreaker
